9654bcfbdaaf7945e233d2522e9cc14402f55cb7
유경호 <ykh6242@gmail.com>
2025-06-19 16:42:00 +0900

    최상단 컴포넌트에 있는 metadata 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,8 @@
-import type { Metadata } from "next";
 import StyledComponentsRegistry from "../lib/registry";
 import { Noto_Sans_KR } from 'next/font/google';
 import "./globals.css";
 
 const notoSansKr = Noto_Sans_KR({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: "나는 어떤 동물 라이더일까?",
-  description: "라이더 유형 검사 powered by Riderly",
-  openGraph: {
-    title: "나는 어떤 동물 라이더일까?",
-    description: "라이더 유형 검사 powered by Riderly",
-    url: "https://riderlybiz.github.io/riderly-mbti/",
-    siteName: "Riderly",
-    images: [
-      {
-        url: "https://riderlybiz.github.io/riderly-mbti/images/open_graph_thumnail.png",
-        width: 1200,
-        height: 600,
-        alt: "Riderly OG Image",
-      },
-    ],
-    type: "website",
-  }
-};
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
 'use client';
 
+import type { Metadata } from "next";
 import { Main } from "../components/Main";
+
+export const metadata: Metadata = {
+  title: "나는 어떤 동물 라이더일까?",
+  description: "라이더 유형 검사 powered by Riderly",
+  openGraph: {
+    title: "나는 어떤 동물 라이더일까?",
+    description: "라이더 유형 검사 powered by Riderly",
+    url: "https://riderlybiz.github.io/riderly-mbti/",
+    siteName: "Riderly",
+    images: [
+      {
+        url: "https://riderlybiz.github.io/riderly-mbti/images/open_graph_thumnail.png",
+        width: 1200,
+        height: 600,
+        alt: "Riderly OG Image",
+      },
+    ],
+    type: "website",
+  }
+};
 
 export default function Home() {
   return (
