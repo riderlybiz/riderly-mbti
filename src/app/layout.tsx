7848558f7fff79338303a23f88743a33ed489e2e
유경호 <ykh6242@gmail.com>
2025-06-20 16:01:07 +0900

    ga 별도 속성으로 연결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={notoSansKr.className} style={{ overflow: "hidden" }}>
-        <GoogleAnalytics gaId="G-9RS5PVKLKX"/>
+        <GoogleAnalytics gaId="G-BCHDZ3EDCE"/>
         <StyledComponentsRegistry>
           {children}
         </StyledComponentsRegistry>
